56d40c966b035db1fb42d4edf009958b3587f038
Carururu <cdvsalanga@gmail.com>
2021-08-26 19:48:57 +0800

    Create User Page

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router-dom'
 
 const CreateUser = () => {
-  return <div className='container'>Create User</div>
+  const { register, handleSubmit } = useForm()
+  const history = useHistory()
+
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data))
+    history.push('/')
+  })
+
+  return (
+    <div className='container'>
+      <div>
+        <h3>Create User</h3>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='username'>Username:</label>
+            <input
+              className='form-control'
+              {...register('username')}
+              type='text'
+              name='username'
+              id='username'
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-primary'>
+              Create User
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default CreateUser
